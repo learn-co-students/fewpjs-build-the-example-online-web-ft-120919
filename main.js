90048727865547ff5acf0d38de38e7e9691d471f
allison_johnson <allison.cuttler@gmail.com>
2020-04-01 18:48:14 -0700

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
+//HTML hexadecimal entity: &#x2661
 const FULL_HEART = '♥'
+//HTML hexadecimal entity: &#x2665
 
 // Your JavaScript code goes here!
+const emptyHearts = document.getElementsByClassName("like-glyph");
+const modal = document.getElementById("modal");
 
+for (const elt of emptyHearts) {
+  elt.addEventListener("click", function(event) {
+    mimicServerCall().then(function(response) {
+      event.target.classList.add("activated-heart")
+      //debugger 
+      toggleHeart(event.target);//Also make heart full
+    }).catch(function(error) {
+      //console.log(error);
+      modal.classList.remove("hidden");
+      modal.querySelector("p").innerText = error;
+      //Use setTimeout to hide the model (add hidden class) after 5 seconds
+      window.setTimeout(function(){modal.classList.add("hidden");}, 5000)
+    })
+    done();
+  })//event listener click empty heart
+}
 
+function toggleHeart(elt) {
+  console.log("Inside toggleHeart")
+  //debugger 
+  if(elt.innerHTML == EMPTY_HEART) {
+    elt.innerHTML = FULL_HEART;
+  }
+  else {
+    elt.innerHTML = EMPTY_HEART;
+    elt.classList.remove("activated-heart")
+  }//if
+}//toggleHeart
 
 
 //------------------------------------------------------------------------------
